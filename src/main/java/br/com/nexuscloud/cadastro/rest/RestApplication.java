b49af00d2ed13e0ee5f2f3175a477453b98acb36
Aguinaldo Fryder <aguinaldo@publitechsistemas.com.br>
2019-12-02 22:06:40 -0300

    Initial commit

---
+++ src/main/java/br/com/nexuscloud/cadastro/rest/RestApplication.java
@@ -0,0 +1,8 @@
+package br.com.nexuscloud.cadastro.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+}
