2eaa1bc7f03ef907190ea3ff058bd2101a81faad
unknown <a.fabianochaves@gmail.com>
2019-12-04 21:35:12 -0300

    Branch Aquinaldo Fabiano

---
+++ src/main/java/br/com/nexuscloud/cadastro/model/tiposPessoas.java
@@ -0,0 +1,23 @@
+package br.com.nexuscloud.cadastro.model;
+
+public class tiposPessoas {
+	private String tipoPessoa;
+
+	public tiposPessoas(String tipoPessoa) {
+		super();
+		this.tipoPessoa = tipoPessoa;
+	}
+	
+	public tiposPessoas() {}
+
+	public String getTipoPessoa() {
+		return tipoPessoa;
+	}
+
+	public void setTipoPessoa(String tipoPessoa) {
+		this.tipoPessoa = tipoPessoa;
+	}
+	
+	
+}
+
--- src/main/java/br/com/nexuscloud/cadastro/rest/HelloWorldEndpoint.java
+++ src/main/java/br/com/nexuscloud/cadastro/rest/HelloWorldEndpoint.java
@@ -3,15 +3,19 @@ package br.com.nexuscloud.cadastro.rest;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+
+import br.com.nexuscloud.cadastro.model.Pessoa;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 
 
 @Path("/hello")
-public class HelloWorldEndpoint {
+public class HelloWorldEndpoint extends Pessoa{
     @GET
     @Produces("text/plain")
     public Response doGet() {
+    	
         return Response.ok("Hello from Thorntail!").build();
     }
 }
